52fe8a90a1927b46f30a6040e4865fcb9df696db
besart951 <besart_morina@hotmail.com>
2026-06-12 17:20:42 +0200

    init

---
+++ packages/ui-svelte/src/lib/index.ts
@@ -0,0 +1,7 @@
+export * as Button from './components/ui/button/index.js';
+export * as Input from './components/ui/input/index.js';
+export * as Label from './components/ui/label/index.js';
+export * as Card from './components/ui/card/index.js';
+export * as Dialog from './components/ui/dialog/index.js';
+export * as DropdownMenu from './components/ui/dropdown-menu/index.js';
+export * as Table from './components/ui/table/index.js';
